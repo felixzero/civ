769fda454fef69612418ff0aa6c825347697655d
Kevin Guilloy <kevin@guilloy.fr>
2020-11-29 17:49:36 +0100

    Add build CI to backend

--- backend/src/test/java/fr/guilloy/civ/CivApplicationTests.java
+++ backend/src/test/java/fr/guilloy/civ/CivApplicationTests.java
@@ -3,7 +3,7 @@ package fr.guilloy.civ;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CivApplication.class)
 class CivApplicationTests {
 
 	@Test
